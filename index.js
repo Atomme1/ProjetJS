516797a4de9f28f38ec57f96d0036fd82eec76bd
trist <tristan@lataniere.net>
2022-04-27 11:39:08 +0200

    index

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 
 const z = require("zebras")
+const avgSpeed = z.pipe([
+    // array of functions for input to travel through
+    z.readCSV,
+    z.parseNums(["speed", "acc"]),
+    z.getCol("speed"),
+    z.mean,
+])("data.csv")
