3ef8bc3c79929f304f72d0c553bde3c20dc870dd
trist <tristan@lataniere.net>
2022-04-27 12:15:34 +0200

    index

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
+const R = require('ramda');
+const Z = require("zebras");
 
-const z = require("zebras")
-const avgSpeed = z.pipe([
+const df = Z.readCSV("CSV_file/Airplane_Crashes_and_Fatalities_Since_1908_2019.csv")
+const datasetLoaded = Z.pipe([
     // array of functions for input to travel through
-    z.readCSV,
-    z.parseNums(["speed", "acc"]),
-    z.getCol("speed"),
-    z.mean,
-])("data.csv")
+    Z.readCSV
+
+])("CSV_file/Airplane_Crashes_and_Fatalities_Since_1908_2019.csv")
+
+console.log(datasetLoaded);
+Z.print(df);
+/*
+
+const data = [
+    {"Date": "1997-01-01", "Value": "12"},
+    {"Date": "1997-01-02", "Value": "14"},
+    {"Date": "1997-01-03", "Value": "7"},
+    {"Date": "1997-01-04", "Value": "112"}
+]
+Z.pipe([
+    Z.parseNums(["Value"]), // converts "Value" column to floats
+    Z.getCol("Value"), // extracts "Value" column to array
+    Z.mean() // calculates mean of "Value" array
+])(data)
+// 36.25
+
+*/
