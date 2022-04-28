60bba68ccfc9bf65ab2b77aebb95aebd551dfea9
rouxe <rouxemilie92@gmail.com>
2022-04-28 11:02:20 +0200

    Fonctionnal function

--- CSV_file/csvToJson.js
+++ CSV_file/csvToJson.js
@@ -4,13 +4,13 @@ import * as fs from 'fs';
 const csv = fs.readFileSync('./CSV_file/Airplane_Crashes_and_Fatalities_Since_1908_2019.csv', 'utf8')
 const columns = ['Date','Time','Location','Operator','Flight #','Route','AC Type','Registration','cn/ln','Aboard','Aboard Passangers','Aboard Crew','Fatalities','Fatalities Passangers','Fatalities Crew','Ground','Summary']
 
-const csvToJson = csvJson => R.compose(
+const csvToJson = (csvfile, listcolumns) => R.compose(
     R.map(R.compose(
-        R.zipObj(columns),
+        R.zipObj(listcolumns),
         R.split(',')
     )),
     R.split('\n')
-)(csv)
+)(csvfile)
 
 export default csvToJson;
 
--- index.js
+++ index.js
@@ -3,7 +3,10 @@ import * as Z from 'zebras';
 import * as fs from 'fs';
 import csvToJson from "./CSV_file/csvToJson.js";
 
-const dataset = csvToJson();
+const csv = fs.readFileSync('./CSV_file/Airplane_Crashes_and_Fatalities_Since_1908_2019.csv', 'utf8')
+const columns = ['Date','Time','Location','Operator','Flight #','Route','AC Type','Registration','cn/ln','Aboard','Aboard Passangers','Aboard Crew','Fatalities','Fatalities Passangers','Fatalities Crew','Ground','Summary']
+
+const dataset = csvToJson(csv,columns);
 console.log(dataset);
 
 
