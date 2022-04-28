13f2a5ce692436ffb9f5ec80e265724a01609d46
rouxe <rouxemilie92@gmail.com>
2022-04-28 11:06:08 +0200

    Change to match new csv

--- CSV_file/csvToJson.js
+++ CSV_file/csvToJson.js
@@ -1,8 +1,4 @@
 import * as R from 'ramda';
-import * as fs from 'fs';
-
-const csv = fs.readFileSync('./CSV_file/Airplane_Crashes_and_Fatalities_Since_1908_2019.csv', 'utf8')
-const columns = ['Date','Time','Location','Operator','Flight #','Route','AC Type','Registration','cn/ln','Aboard','Aboard Passangers','Aboard Crew','Fatalities','Fatalities Passangers','Fatalities Crew','Ground','Summary']
 
 const csvToJson = (csvfile, listcolumns) => R.compose(
     R.map(R.compose(
--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+//libraries import
 import * as R from 'ramda';
 import * as Z from 'zebras';
 import * as fs from 'fs';
+
+//functions import
 import csvToJson from "./CSV_file/csvToJson.js";
 
-const csv = fs.readFileSync('./CSV_file/Airplane_Crashes_and_Fatalities_Since_1908_2019.csv', 'utf8')
-const columns = ['Date','Time','Location','Operator','Flight #','Route','AC Type','Registration','cn/ln','Aboard','Aboard Passangers','Aboard Crew','Fatalities','Fatalities Passangers','Fatalities Crew','Ground','Summary']
+//details of the csv to analyze
+const csv = fs.readFileSync('./CSV_file/Iris.csv', 'utf8')
+const columns = ['Id','SepalLengthCm','SepalWidthCm','PetalLengthCm','PetalWidthCm','Species']
 
+//analyze of the csv
 const dataset = csvToJson(csv,columns);
 console.log(dataset);
 
