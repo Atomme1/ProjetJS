7c2a1180b651a899b3dcec6333fb7ca0c77f9ed7
norbertastier <81748722+norbertastier@users.noreply.github.com>
2022-04-29 14:47:34 +0200

    Update createTwoDatasets.js

--- createTwoDatasets.js
+++ createTwoDatasets.js
@@ -3,29 +3,39 @@ import * as fs from 'fs';
 
 import { csvToJson} from "./functionUtils.js";
 
-const firstOrNull = R.prop(0)
+const csv = fs.readFileSync('./CSV_file/Iris.csv', 'utf8')
+const columns = ['Id','SepalLengthCm','SepalWidthCm','PetalLengthCm','PetalWidthCm','Species']
 
-const getCSV = (path) => fs.readFileSync(path).toString().split('\r\n')
+const dataset = csvToJson(csv,columns);
 
-const getCSV2 = (path) => fs.readFileSync(path, 'utf8')
+const getFirstLine = R.prop(0)
 
-const dropColumns = (list) => R.drop(1,list)
+//const getCSV = (path) => fs.readFileSync(path).toString().split('\r\n')
 
-const getColumns = (path) => firstOrNull(getCSV(path))
+const getCSV = R.pipe(
+    fs.readFileSync,
+    R.toString,
+    R.split('\r\n')
+)
+
+const getCSV2 = (path) => fs.readFileSync(path,'utf8');
+
+const dropColumns =  R.drop(1,R.identity)
+
+const getColumns = R.converge(getFirstLine,[getCSV])
 
 const getColumnsToList = (str) => str.split(",")
 
 
 //console.log(dropColumns(dataset))
 
-console.log(getColumns('./CSV_file/Iris.csv'))
-console.log(getColumnsToList(getColumns('./CSV_file/Iris.csv')))
+//console.log(getColumns('./CSV_file/Iris.csv'))
+//console.log(getColumnsToList(getColumns('./CSV_file/Iris.csv')))
 
 const path = './CSV_file/Iris.csv'
+console.log(getCSV2(path))
+//const data = csvToJson(getCSV2(path),getColumnsToList(getColumns(path)))
 
-const data = csvToJson(getCSV2(path),getColumnsToList(getColumns(path)))
-
-console.log(dropColumns(data))
+//console.log(dropColumns(data))
 
 //console.log(dataset)
-
