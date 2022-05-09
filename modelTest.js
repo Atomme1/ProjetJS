b00cfeeed44aa3e8effbdae5822bd940f8002672
trist <tristan@lataniere.net>
2022-05-09 15:16:58 +0200

    model knn

--- modelTest.js
+++ modelTest.js
@@ -1,30 +1,31 @@
 //libraries import
 import * as R from 'ramda';
 import * as fs from 'fs';
+import { getValues} from "./PCAFunctionUtils.js";
+import { KNeighborsRegressor } from 'scikitjs';
 
 //functions import
 import { csvToJson, getTraining, getTesting, dropColumns, doShuffle} from "./datasetFunctionUtils.js";
-import { getValues, getVectors, first, topTwo} from "./PCAFunctionUtils.js"
-
 //details of the csv to analyze
 const csv = fs.readFileSync('./CSV_file/Iris.csv', 'utf8')
 const columns = ['Id','SepalLengthCm','SepalWidthCm','PetalLengthCm','PetalWidthCm','Species']
-
 //analysis of the csv
 const datasetinit = csvToJson(csv,columns);
-
 const getDataset = R.pipe(
     dropColumns,
     doShuffle,
 );
-
 const dataset = getDataset(datasetinit);
 
-console.log(dataset);
+const getLabels = R.pipe(
+    R.map(R.last)
+)
 
-console.log(getTraining(dataset));
-console.log(getTesting(dataset));
+let X = [[0], [1], [2], [3]];
+let y = [0, 0, 1, 1];
 
-console.log(topTwo(dataset));
+let knn = new KNeighborsRegressor(nNeighbor);
 
+await knn.fit(X, y);
 
+knn.predict([[1.5]]).print();
